c4c0733a22464cb5f8d91435c5033187c47172b7
Mathilda Holmström <mathilda.holmstrom@telia.com>
2023-12-07 22:18:54 +0100

    Add base js

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ export const drawRandomName = (names) => {
     const maxLength = names?.length ?? 0;
     const randomNumber = Math.floor(Math.random() * maxLength);
 
+    console.log(names[randomNumber]);
+
     return names[randomNumber];
 }
 
@@ -9,6 +11,7 @@ export const createNameArray = (userInput) => {
     if (userInput ||userInput.length > 0){
         return 'Inga namn finns!';
     }
+    console.log(userInput.split(', '));
 
     return userInput.split(', ');
 }
