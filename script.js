954f924f8c458392077255a8614c598c6ddbc471
Stephanie Karlsson <Steffihenke@hotmail.se>
2023-12-11 17:52:59 +0100

    ändrat lite css + slumpar namn (visas i konsol)

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ export const drawRandomName = (names) => {
 }
 
 export const createNameArray = (userInput) => {
-    if (userInput ||userInput.length > 0){
+    if (!userInput ||userInput.length === 0){
         return 'Inga namn finns!';
     }
     console.log(userInput.split(', '));
@@ -21,3 +21,12 @@ export const randomizer = (userInput) => {
 
     drawRandomName(nameArray);
 }
+
+const generateNameBtn = document.querySelector('.btn');
+
+generateNameBtn.addEventListener ('click', () => {
+    const userInput = document.querySelector('#exampleFormControlInput1').value;
+    randomizer(userInput);
+});
+
+// Exempel 1, Exempel 2, Exempel 3, Exempel 4, Exempel 5, Exempel 6, Exempel 7, Exempel 8, Exempel 9, Exempel 10, Exempel 11, Exempel 12
